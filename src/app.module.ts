58a1fa66d12a3358167fdad8d289fea6b7d26ef2
Unknown <ctch5@mail.umsl.edu>
2018-05-18 19:20:34 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './shared/shared.module';
+<<<<<<< Updated upstream
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigService } from './shared/services/config.service';
+=======
+<<<<<<< HEAD
+=======
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from './shared/services/config.service';
+>>>>>>> 2fd3d22... intial
+>>>>>>> Stashed changes
 import { SlackModule } from './slack/slack.module';
 import { NestRoutingModule } from './routes';
 import { MessageModule } from './message/message.module';
 
 @Module({
+<<<<<<< Updated upstream
   imports: [SharedModule, NestRoutingModule, MongooseModule.forRoot(ConfigService.connectionString), SlackModule, MessageModule],
+=======
+<<<<<<< HEAD
+  imports: [SharedModule, NestRoutingModule, SlackModule, MessageModule],
+=======
+  imports: [SharedModule, NestRoutingModule, MongooseModule.forRoot(ConfigService.connectionString), SlackModule, MessageModule],
+>>>>>>> 2fd3d22... intial
+>>>>>>> Stashed changes
 })
 export class AppModule {
 }
--- src/shared/services/config.service.ts
+++ src/shared/services/config.service.ts
@@ -5,11 +5,26 @@ import { readFileSync } from 'fs';
 @Injectable()
 export class ConfigService {
   private readonly envConfig: { [prop: string]: string };
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+
+  // static connectionString: string;
+
+  constructor(filePath: string) {
+    this.envConfig = parse(readFileSync(filePath));
+    // ConfigService.connectionString = this.get('MONGO_URI');
+=======
+>>>>>>> Stashed changes
   static connectionString: string;
 
   constructor(filePath: string) {
     this.envConfig = parse(readFileSync(filePath));
     ConfigService.connectionString = this.get('MONGO_URI');
+<<<<<<< Updated upstream
+=======
+>>>>>>> 2fd3d22... intial
+>>>>>>> Stashed changes
   }
 
   get(key: string): string {
