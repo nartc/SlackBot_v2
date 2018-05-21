4bb80587c691e3ca99d0afa9237abafa3ad5cbf9
Unknown <ctch5@mail.umsl.edu>
2018-05-21 10:02:35 -0500

    test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
